6319693e246c603584c0d4903a00297af5becfdf
David Morley <morleyd@google.com>
2020-06-17 21:00:52 +0000

    Added a contradiction

--- hello.py
+++ hello.py
@@ -2,5 +2,5 @@ print('Hello, this is Python')
 print('2+ 2 is %d' %(2+2))
 
 def my_function():
-    '''This will do something cool.'''
-    return None
+    '''This will do something not great.'''
+    return 0
